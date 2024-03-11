6713e6c67b5150a25516ff85c89802b1e7ae333a
Amias Yaska <amiasyaska@gmail.com>
2024-03-11 15:55:40 +0300

    changes in items

--- bizbuyscraper/bizbuyscraper/items.py
+++ bizbuyscraper/bizbuyscraper/items.py
@@ -4,9 +4,44 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import TakeFirst
 
 
 class BizbuyscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    header = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    image_url = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    description = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    price = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    location = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    broker_company = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    broker_name = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    broker_phone_number = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    broker_photo_url = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+
