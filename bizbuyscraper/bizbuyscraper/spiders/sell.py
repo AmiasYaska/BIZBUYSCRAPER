ec5373e4004cd1dde918dacc3da6c76cb2f17d57
Amias Yaska <amiasyaska@gmail.com>
2024-03-13 14:49:30 +0300

    changes

--- bizbuyscraper/bizbuyscraper/spiders/sell.py
+++ bizbuyscraper/bizbuyscraper/spiders/sell.py
@@ -8,68 +8,26 @@ class SellSpider(scrapy.Spider):
     allowed_domains = ["bizbuysell.com"]
 
     def start_requests(self):
+        page1_body = {
+            "bfsSearchCriteria": {
+                "siteId": 20,
+                "languageId": 10,
+                "seoName": "/california-businesses-for-sale/",
+                "queryString": "q=bHQ9MzAsNDAsODA%3D"
+            },
+            "industriesHierarchy": 0,
+            "industriesFlat": 0,
+            "bfsSearchResultsCounts": 0,
+            "cmsFilteredData": 0,
+            "rightRailBrokers": 0,
+            "statesRegions": 10,
+            "languageTypeId": 10
+        }
 
-        for page_number in range(1, 110):
-            url_body = {
-                "bfsSearchCriteria": {
-                    "siteId": 20,
-                    "languageId": 10,
-                    "categories": [],
-                    "locations": [
-                        {
-                            "geoType": 20,
-                            "regionId": "5",
-                            "countryCode": "US",
-                            "countryId": "US",
-                            "stateCode": "CA",
-                            "legacyRegionId": 18,
-                            "legacyRegionCode": "CA",
-                            "metroAreaId": 0,
-                            "regionName": "California",
-                            "regionNameSeo": "california",
-                            "displayName": "California",
-                            "geoPinCriteria": []
-                        }
-                    ],
-                    "excludeLocations": [],
-                    "askingPriceMax": 0,
-                    "askingPriceMin": 0,
-                    "pageNumber": page_number,
-                    "keyword": [],
-                    "cashFlowMin": 0,
-                    "cashFlowMax": 0,
-                    "grossIncomeMin": 0,
-                    "grossIncomeMax": 0,
-                    "daysListedAgo": 0,
-                    "establishedAfterYear": 0,
-                    "listingsWithNoAskingPrice": 0,
-                    "homeBasedListings": 0,
-                    "includeRealEstateForLease": 0,
-                    "listingsWithSellerFinancing": 0,
-                    "realEstateIncluded": 0,
-                    "showRelocatableListings": False,
-                    "relatedFranchises": 0,
-                    "listingTypeIds": [
-                        30,
-                        40,
-                        80
-                    ],
-                    "designationTypeIds": [],
-                    "sortList": [],
-                    "absenteeOwnerListings": 0
-                },
-                "industriesHierarchy": 10,
-                "industriesFlat": 10,
-                "bfsSearchResultsCounts": 0,
-                "cmsFilteredData": 0,
-                "rightRailBrokers": 0,
-                "statesRegions": 10,
-                "languageTypeId": 10
-            }
         yield scrapy.Request(
             url="https://api.bizbuysell.com/bff/v2/BbsBfsSearchResults",
             method="POST",
-            body=json.dumps(url_body),
+            body=json.dumps(page1_body),
             headers={
                 "Content-Type": "application/json"
             },
@@ -79,18 +37,19 @@ class SellSpider(scrapy.Spider):
     def parse(self, response):
         data = json.loads(response.body)
         values = data.get("value").get("bfsSearchResult").get("value")
+        print(values)
 
-        for value in values:
-            biz_buy_sell = BizbuyscraperItem()
-
-            biz_buy_sell["header"] = value["header"]
-            biz_buy_sell["image_url"] = value["img"]
-            biz_buy_sell["description"] = value["description"]
-            biz_buy_sell["price"] = value["price"]
-            biz_buy_sell["business_location"] = value["location"]
-            biz_buy_sell["broker_company"] = value["contactInfo"]["brokerCompany"]
-            biz_buy_sell["broker_name"] = value["contactInfo"]["contactFullName"]
-            biz_buy_sell["broker_phone_number"] = value["contactInfo"]["contactPhoneNumber"]["telephone"]
-            biz_buy_sell["broker_photo_url"] = value["contactInfo"]["contactPhoto"]
-
-            yield biz_buy_sell
+        # for value in values:
+        #     biz_buy_sell = BizbuyscraperItem()
+        #
+        #     biz_buy_sell["header"] = value["header"]
+        #     biz_buy_sell["image_url"] = value["img"]
+        #     biz_buy_sell["description"] = value["description"]
+        #     biz_buy_sell["price"] = value["price"]
+        #     biz_buy_sell["business_location"] = value["location"]
+        #     biz_buy_sell["broker_company"] = value["contactInfo"]["brokerCompany"]
+        #     biz_buy_sell["broker_name"] = value["contactInfo"]["contactFullName"]
+        #     biz_buy_sell["broker_phone_number"] = value["contactInfo"]["contactPhoneNumber"]["telephone"]
+        #     biz_buy_sell["broker_photo_url"] = value["contactInfo"]["contactPhoto"]
+        #
+        #     yield biz_buy_sell
